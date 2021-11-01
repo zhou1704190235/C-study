c22c5e1d4190a9bcdd502be1615218821e3c5c55
zhou1704190235 <zhou1704190235@163.com>
2021-11-01 20:35:44 +0800

    提交代码为了演示Github的使用

---
+++ Project2.1/fathm_ft.c
@@ -0,0 +1,11 @@
+//fathm_ft.c --把2英寻转化为英尺
+#include<stdio.h>
+int main(void)
+{
+	int feet, fathoms;
+	fathoms = 2;
+	feet = 6 * fathoms;
+	printf("There are %d feet in %d fathomds !\n", feet, fathoms);
+	printf("Yes,I said %d feet !\n", 6 * fathoms);
+	return 0;
+}
